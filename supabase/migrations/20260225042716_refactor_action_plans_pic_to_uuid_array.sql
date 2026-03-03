4da9f138afa5bab9d367a61d4432fa994697848d
werkudaraevent-eng <werkudara.event@gmail.com>
2026-03-03 11:27:04 +0700

    fix: make add column pic_ids idempotent

--- supabase/migrations/20260225042716_refactor_action_plans_pic_to_uuid_array.sql
+++ supabase/migrations/20260225042716_refactor_action_plans_pic_to_uuid_array.sql
@@ -15,7 +15,7 @@ END $$;
 
 -- 2. Menambahkan kolom baru berwujud Array untuk menampung banyak UUID staf
 -- Kita memberikan nilai default array kosong '{}' agar tidak terjadi error saat query
-ALTER TABLE public.action_plans ADD COLUMN pic_ids uuid[] DEFAULT '{}'::uuid[];
+ALTER TABLE public.action_plans ADD COLUMN IF NOT EXISTS pic_ids uuid[] DEFAULT '{}'::uuid[];
 
 -- 3. Menambahkan komentar arsitektur agar developer masa depan paham
 COMMENT ON COLUMN public.action_plans.legacy_pic_text IS 'Data mentah dari import awal. Jangan gunakan untuk data baru.';
